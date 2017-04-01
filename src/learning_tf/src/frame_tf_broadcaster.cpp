3910e8a0dde3caeac429681fd84cd44c4f32f495
Parker Lusk <parkerclusk@gmail.com>
2017-04-01 17:21:37 -0600

    moving carrot

--- src/learning_tf/src/frame_tf_broadcaster.cpp
+++ src/learning_tf/src/frame_tf_broadcaster.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 
+#define MOVING_FRAME
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "my_frame_tf_broadcaster");
     ros::NodeHandle nh;
@@ -13,6 +15,10 @@ int main(int argc, char** argv) {
         // Create the carrot, always to the right 2 units
         transform.setOrigin( tf::Vector3(0.0, 2.0, 0.0) );
         transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
+
+#ifdef MOVING_FRAME
+        transform.setOrigin( tf::Vector3(2.0*sin(ros::Time::now().toSec()), 2.0*cos(ros::Time::now().toSec()), 0.0) );
+#endif
 
         // send the tf: Parent -- turtle1; Child -- carrot1
         br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "turtle1", "carrot1"));
