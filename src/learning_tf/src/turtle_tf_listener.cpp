885ddd9b4056ccf6779f4ebf284fb64dde3e70c7
Parker Lusk <parkerclusk@gmail.com>
2017-04-01 17:37:45 -0600

    time travel with tf

--- src/learning_tf/src/turtle_tf_listener.cpp
+++ src/learning_tf/src/turtle_tf_listener.cpp
@@ -3,6 +3,8 @@
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Spawn.h>
 
+#define TF_EXAMPLE 4
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "my_tf_listener_node");
     ros::NodeHandle nh;
@@ -28,13 +30,33 @@ int main(int argc, char** argv) {
             // Change this `1` to a `0` if you want to use the carrot to control
             std::string target_frame = (1) ? "/turtle1" : "/carrot1";
 
+#if TF_EXAMPLE == 1
             // ros::Time(0) just says "give me the latest"
-            // listener.lookupTransform("/turtle2", target_frame, ros::Time(0), transform);
+            listener.lookupTransform("/turtle2", target_frame, ros::Time(0), transform);
 
+#elif TF_EXAMPLE == 2
             // Instead of getting the latest tf, let's wait
             ros::Time now = ros::Time::now();
-            listener.waitForTransform("/turtle2", target_frame, now, ros::Duration(3.0));
+            listener.waitForTransform("/turtle2", target_frame, now, ros::Duration(1.0));
             listener.lookupTransform("/turtle2", target_frame, now, transform);
+
+#elif TF_EXAMPLE == 3
+            // Control current pose of turtle2 based on old pose between turtle2 and turtle1.
+            // This results in uncontrollable motion.
+            ros::Time past = ros::Time::now() - ros::Duration(5.0);
+            listener.waitForTransform("/turtle2", target_frame, past, ros::Duration(1.0));
+            listener.lookupTransform("/turtle2", target_frame, past, transform);
+
+#elif TF_EXAMPLE == 4
+            // Control current pose of turtle2 based on current turtle2 and old turtle1.
+            ros::Time now = ros::Time::now();
+            ros::Time past = now - ros::Duration(5.0);
+
+            // The world frame must be specified as the frame that does not change over time
+            listener.waitForTransform("/turtle2", now, target_frame, past, "/world", ros::Duration(1.0));
+            listener.lookupTransform("/turtle2", now, target_frame, past, "/world", transform);
+
+#endif
 
             // source (from): "/turtle2"
             // target   (to): "/turtle1"
